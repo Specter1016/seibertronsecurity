1bf29cadbeaac03c9a9414251d47aa89c57f7815
Specter1016 <base_1996@hotmail.co.th>
2020-03-21 05:23:17 +0700

    add full 3

--- src/main/java/com/seibertronsecurity/seibertronsecurity/controller/HomeController.java
+++ src/main/java/com/seibertronsecurity/seibertronsecurity/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.seibertronsecurity.seibertronsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.Map;
@@ -13,7 +14,7 @@ public class HomeController {
 
     private String message ="Seibertron Security";
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
         model.put("message", this.message);
         return "index";
