8284a081b194620d8a4b7d98d0462cc24b76959d
Specter1016 <base_1996@hotmail.co.th>
2020-03-21 13:48:26 +0700

    add componant

--- src/main/java/com/seibertronsecurity/seibertronsecurity/controller/HomeController.java
+++ src/main/java/com/seibertronsecurity/seibertronsecurity/controller/HomeController.java
@@ -1,11 +1,11 @@
 package com.seibertronsecurity.seibertronsecurity.controller;
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-import java.util.Map;
 
 @Controller
 public class HomeController {
